b8111b8ef03b21bd89fc47d58339ac340eee8505
fatmonk <ifatmonk@gmail.com>
2012-07-21 16:41:28 -0700

    recomit

---
+++ getit2012/GetIt12.h
@@ -0,0 +1,13 @@
+//
+//  GetIt12.h
+//  GetIt
+//
+//  Created by Luong Tam on 7/21/12.
+//  Copyright (c) 2012 GetIt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GetIt12 : NSObject
+
+@end
---
+++ getit2012/GetIt12.m
@@ -0,0 +1,13 @@
+//
+//  GetIt12.m
+//  GetIt
+//
+//  Created by Luong Tam on 7/21/12.
+//  Copyright (c) 2012 GetIt. All rights reserved.
+//
+
+#import "GetIt12.h"
+
+@implementation GetIt12
+
+@end
